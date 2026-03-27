e6502112778cc89b0c40b237ea784b54ef772463
Meu Nome <joelma.carmo.duarte@gmail.com>
2026-03-27 11:12:36 -0300

    Exercício Minhas Frutas Preferidas concluído

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine();
+Console.WriteLine("♥ Minhas frutas preferidas ♥:");
+Console.WriteLine();
+Console.WriteLine("1. Banana");
+Console.WriteLine("2. Laranja");
+Console.WriteLine("3. Melancia");
+Console.WriteLine("4. Mamão");
+Console.WriteLine("5. Melão");
+Console.WriteLine();
